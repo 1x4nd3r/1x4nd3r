f0acdda251712b4ea4de666f9a9d931e3cf2ec9c
Alexander <alex.stille@proton.me>
2025-05-21 23:17:17 +0200

    000

---
+++ logo.js
@@ -0,0 +1,9 @@
+let logo = document.getElementById("logo");
+
+logo.addEventListener("mouseover", function () {
+    logo.innerText = "^_^";
+});
+
+logo.addEventListener("mouseout", function () {
+    logo.innerText = "X_X";
+});
